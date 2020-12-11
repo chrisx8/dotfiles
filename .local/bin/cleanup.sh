52541e58b87ba2ad80b0aa17df93bda1824f1390
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2020-12-10 23:04:54 -0500

    fix path in cleanup

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -2,18 +2,18 @@
 
 echo '##### Cleaning cache... #####'
 rm -rf ~/.cache/*
-rm -rf ".config/Code - OSS/Cache"
-rm -rf ".config/Code - OSS/CachedData"
-rm -rf ".config/Code - OSS/CachedExtensions"
-rm -rf ".config/Code - OSS/CachedExtensionVSIXs"
-rm -rf ".config/Code - OSS/Code Cache"
-rm -rf ".config/Code - OSS/GPUCache"
-rm -rf ".config/Signal/Cache"
-rm -rf ".config/Signal/Code Cache"
-rm -rf ".config/Signal/GPUCache"
-rm -rf ".config/Signal/temp"
+rm -rf "~/.config/Code - OSS/Cache"
+rm -rf "~/.config/Code - OSS/CachedData"
+rm -rf "~/.config/Code - OSS/CachedExtensions"
+rm -rf "~/.config/Code - OSS/CachedExtensionVSIXs"
+rm -rf "~/.config/Code - OSS/Code Cache"
+rm -rf "~/.config/Code - OSS/GPUCache"
+rm -rf "~/.config/Signal/Cache"
+rm -rf "~/.config/Signal/Code Cache"
+rm -rf "~/.config/Signal/GPUCache"
+rm -rf "~/.config/Signal/temp"
 rm -rf ~/.gnome
-rm -rf .local/share/gvfs-metadata
+rm -rf ~/.local/share/gvfs-metadata
 rm -rf ~/.local/share/virtualenv
 rm -rf ~/.npm   
 rm -rf ~/.pylint.d
