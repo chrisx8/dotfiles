b33c2692e0b0dd355f420fa37d6d5e316ffbc01d
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-01-01 18:25:30 -0500

    fix style

--- .local/bin/upgrade-pip-venv.sh
+++ .local/bin/upgrade-pip-venv.sh
@@ -2,7 +2,7 @@
 
 if [[ -f requirements.txt ]] && [[ -d "$VIRTUAL_ENV" ]]; then
 	echo "Upgrading all outdated packages..."
-	pip install -U `pip list --outdated --format=freeze | cut -d = -f 1`
+	pip install -U $(pip list --outdated --format=freeze | cut -d = -f 1)
 	pip freeze > requirements.txt
 else
 	echo "No requirements.txt or activated virtual environment. Exiting..."
