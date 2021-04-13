9511453fdcb8945410aabd8a1f3181957ca41397
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2021-04-13 18:32:52 -0400

    add network reset script

---
+++ .local/bin/networkreset.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+if [[ "$@" == "--config" ]]; then
+	echo "Please disconnect from VPNs before continuing."
+	read -p "Press Enter to continue..."
+	echo
+fi
+sudo -v
+
+echo "Resetting DNS..."
+sudo rm -f /etc/resolv.conf.*
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+sudo systemctl restart systemd-resolved.service
+
+echo "Enabling IPv6..."
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=0 > /dev/null
+
+echo "Restarting NetworkManager..."
+sudo systemctl restart NetworkManager.service
+
+echo "OK"
