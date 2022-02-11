daff1ac38de995bc5a64e1c2f6cce9549f7b6bed
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2022-02-10 23:54:31 -0500

    remove cryptreferesh script

--- .local/bin/cryptrefresh.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo clevis luks bind -d /dev/nvme0n1p2 tpm2 '{"pcr_ids":"0,2,7"}'
-#sudo systemd-cryptenroll /dev/nvme0n1p2 --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs=0,2,4,7
