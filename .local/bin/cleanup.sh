3c62e4d31c1a78957c7aa68e66ac6ee46257ac89
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2022-02-14 20:39:49 -0500

    trim spaces

--- .local/bin/cleanup.sh
+++ .local/bin/cleanup.sh
@@ -10,7 +10,7 @@ rm -rf ~/.gnome
 rm -rf ~/.local/share/gvfs-metadata
 rm -rf ~/.local/share/RecentDocuments
 rm -rf ~/.local/share/virtualenv
-rm -rf ~/.npm   
+rm -rf ~/.npm
 rm -rf ~/.oh-my-zsh/cache/*
 rm -rf ~/.pylint.d
 rm -rf ~/.var/app/*/cache/*
