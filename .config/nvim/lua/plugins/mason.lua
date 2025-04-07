59e7fe1d2ff2f4f095dea23b72935a4ba2a22a4e
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2025-04-06 23:14:36 -0500

    astronvim v5

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,48 +1,26 @@
--- Customize Mason plugins
-
 ---@type LazySpec
 return {
-  -- use mason-lspconfig to configure LSP installations
+  -- use mason-tool-installer for automatically installing Mason packages
   {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "ansiblels",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
+    opts = {
+      -- Make sure to use the names found in `:Mason`
+      ensure_installed = {
+        "ansible-language-server",
         "basedpyright",
-        "bashls",
-        "lua_ls",
-        "rust_analyzer",
-      })
-    end,
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "bash-language-server",
         "black",
         "isort",
+        "lua-language-server",
         "markdownlint",
         "prettier",
+        "rust-analyzer",
         "shellcheck",
         "shfmt",
         "stylua",
         "yamllint",
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        -- add more arguments for adding more debuggers
-      })
-    end,
+      },
+    },
   },
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,11 @@
--- Customize Treesitter
-
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    opts.auto_install = true
-    opts.ignore_install = {
+  opts = {
+    auto_install = true,
+    ignore_install = {
       "git_rebase",
       "gitcommit",
-    }
-  end,
+    },
+  },
 }
